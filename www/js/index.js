8ef57c0e2d9681669bfcee7a585c304a8caf6f70
Peter Sloots <git@pjeutr.com>
2015-05-01 16:08:23 +0200

    fix current

--- www/js/index.js
+++ www/js/index.js
@@ -67,7 +67,7 @@ function loadChildBrowser(isInternal, URL) {
                     var strPath = window.location.href; 
                     var path = strPath.substr(0,strPath.lastIndexOf('/')) + '/' + URL; 
 					console.log(encodeURI(path));
-					window.plugins.childBrowser.showWebPage(encodeURI(path));
+					window.plugins.ChildBrowser.showWebPage(encodeURI(path));
                     //Cordova.exec("ChildBrowserCommand.showWebPage", encodeURI(path) ); 
             } 
             else{ 
